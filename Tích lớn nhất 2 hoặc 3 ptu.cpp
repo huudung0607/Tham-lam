9da5d9f8c56f2432ff475df04e625068a8b57e13
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-06-12 09:14:16 +0700

    Create Tích lớn nhất 2 hoặc 3 ptu.cpp

---
+++ Tích lớn nhất 2 hoặc 3 ptu.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	int n; cin >> n;
+	vector<int>a(n);
+	for (int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	long long res = LLONG_MIN;
+	res = max(res, 1ll*a[0] * a[1]);             
+	res = max(res, 1ll * a[n - 1] * a[n - 2]);
+	res = max(res,1ll* a[0] * a[1] * a[n - 1]);    
+	res = max(res, 1ll* a[n - 1] * a[n - 2] * a[n - 3]);
+	cout << res;
+}
+
